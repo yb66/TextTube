8e8e8b2d6c303b140fe1c4fd6b4cad5321400d55
Iain Barnett <iainspeed@gmail.com>
2011-06-14 08:37:55 +0100

    added version number

--- lib/markdownfilters.rb
+++ lib/markdownfilters.rb
@@ -1,5 +1,9 @@
 # encoding: UTF-8
 
+module MarkdownFilters
+  VERSION = "0.1.0"
+end
+
 require_relative "./markdownfilters/link_reffing.rb"
 require_relative "./markdownfilters/embedding_video.rb"
 require_relative "./markdownfilters/embedding_audio.rb"
