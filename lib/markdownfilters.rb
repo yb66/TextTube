e047c265028ddb90a746fe9feefcf37c8a84f7da
Iain Barnett <iainspeed@gmail.com>
2013-06-03 18:50:42 +0100

    I want individual filters to be loaded via requires in the client code. That removes the need for configuration at the other end.

--- lib/markdownfilters.rb
+++ lib/markdownfilters.rb
@@ -1,8 +1,3 @@
 # encoding: UTF-8
 
-
-require_relative "./markdownfilters/link_reffing.rb"
-require_relative "./markdownfilters/embedding_video.rb"
-require_relative "./markdownfilters/embedding_audio.rb"
-require_relative "./markdownfilters/inside_block.rb"
-require_relative "./markdownfilters/coderay.rb"
+require_relative "./ext/blank.rb"
