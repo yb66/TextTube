1684660db8a4ddf5e43ae06f263e2a1820447962
Iain Barnett <iainspeed@gmail.com>
2013-06-06 00:50:00 +0100

    Link reffing works like it did before, but with better specs, wraps the div properly, and the code is a bit cleaner and more idiomatic;

--- lib/markdownfilters/filters/link_reffing.rb
+++ lib/markdownfilters/filters/link_reffing.rb
@@ -8,75 +8,93 @@ module MarkdownFilters
     
     # These are the html codes for superscript 0 - 9
     UNITS = ['&#8304;', '&sup1;', '&sup2;', '&sup3;', '&#8308;', '&#8309;', '&#8310;', '&#8311;', '&#8312;', '&#8313;'] #unicode superscript numbers
+
+    # Matches [[link|description]]
+    Pattern = /              
+          \[\[                      # opening square brackets
+            (?<link>\S+)
+              \s*\|\s*              # separator
+            (?<description>[^\[]+)
+          \]\]                      # closing square brackets
+        /x
     
     # a lambda function to transform a link and a number into a markdown reference link
     # @param [String] lnk The url.
     # @param [String] num The reference number.
     Markdowner = ->(lnk, num){ %Q![#{lnk}](##{num} "Jump to reference")! }
+
+    HTMLer = ->(lnk, num){ %Q!<a href="#{lnk}" title="Jump to reference">#{num}</a>!  }
     
     # Takes markdown content with ref links and turns it into 100% markdown.
     # @param [String] content The markdown content with links to ref.
+    # @option options [#to_s] :format The format of the link you want added. The options are :html, :markdown. The default is :markdown
+    # @option options [#to_s] :type The kind of link you want added. The options are :reference, :normal, :none. The default is :reference
+    # @option options [String,nil] :div_id ID of the div to wrap reference links in. Defaults to "reflinks". Set to nil or false for no div.
+    # @return [String] The string formatted as markdown e.g. `[http://cheat.errtheblog.com/s/yard/more/and/m...](http://cheat.errtheblog.com/s/yard/more/and/more/and/more/ "http://cheat.errtheblog.com/s/yard/more/and/more/and/more/")`
     def self.run(content, options={})
+      return content if content.blank?
       options ||= {}
+      type = options.fetch :type, :reference
+      format = case options.fetch( :format, :markdown )
+        when :html then HTMLer
+        when :markdown then Markdowner
+        else Markdowner
+      end
+      div_id =  options.has_key?(:div_id) ? 
+                  options[:div_id] :
+                  :reflinks
+
       cur = 0 #current number
-      links = [ ] #to store the matches
+
+      # if there are no reflinks found
+      # this will remain false
+      # and `divit` won't be run.
+      has_reflinks = false
+      links = [] #to store the matches
       
-      blk = ->(m) do  #block to pass to gsub
-        links << [$1, $2] # add to the words list
+      content.gsub! Pattern do |md|  #block to pass to gsub
+        has_reflinks = true
         mags = cur.divmod(10) #get magnitude of number
-        ref_tag = mags.first >= 1 ? UNITS[mags.first] : '' #sort out tens
+        ref_tag = mags.first >= 1 ? 
+                    UNITS[mags.first] :
+                    '' #sort out tens
+
         ref_tag += UNITS[mags.last] #units
         
-        
-        format = options[:format].nil? ? Markdowner : options[:format] # markdown is the default format
         retval = format.(ref_tag,cur)
         cur = cur + 1 #increase current number
-    
+        links << [$1, $2, cur] # add to the words list
         retval
       end
-      
-      r = /              # [[link|description]]
-            \[\[         # opening square brackets
-              (\S+)      # link
-                \s*\|\s* # separator
-              ([^\[]+)   # description
-            \]\]         # closing square brackets
-          /x 
-      
-      if r.match content
-        replacement = content.gsub( r, &blk ) + "\n"
-        replacement + LinkReffing.format_links(links) unless links.empty?
+
+      if !links.empty?
+        if div_id
+          "#{content}\n#{LinkReffing.divit( div_id ) { format_links(links) }}"
+        else
+          "#{content}\n#{format_links(links)}"
+        end
       else
         content
       end
-      
     end
-      
-    # This func outputs the link as valid HTML.
-    # @param [Hash] options Options hash.
-    # @param [Array<String,String>] links A list of 2-length arrays containing the url and the description.
-    # @option options [true,false] :top_html_rule Whether to put a horizontal rule across the top of the link.
-    # @option options [true,false] :bottom_html_rule Whether to put a horizontal rule across the bottom of the link.
-    # @option options [String,nil] :div Name of the div to wrap reference links in. Defaults to "reflinks". Set to nil for no div.
-    # @return [String] The string formatted as markdown e.g. `[http://cheat.errtheblog.com/s/yard/more/and/m...](http://cheat.errtheblog.com/s/yard/more/and/more/and/more/ "http://cheat.errtheblog.com/s/yard/more/and/more/and/more/")`
-    def self.format_links( links, options={div: "reflinks"} )
-      text = ""
-      cur = 0
-      links.each do |lnk|
-        display_link = lnk.first.length >= 45 ? 
-                          lnk.first[0,45] + "..." : 
-                          lnk.first
-        text += %Q!\n<a name="#{cur}"></a>#{LeftSq}#{cur}#{RightSq} [#{display_link}](#{lnk.first} "#{lnk.first}") #{lnk.last}\n\n!
-        cur += 1
-      end
-      LinkReffing.divit( options[:div] ) do
-        text
-      end
+
+
+    # This func outputs the link as valid markdown.
+    # @param [Array<String,String,Integer>] links A list of 2-length arrays containing the url and the description and the reference number.
+    def self.format_links( links )
+      links.map{ |(link, description, cur)|
+        display_link = link.length >= 45 ? 
+                          link[0,45] + "..." : 
+                          link
+        %Q!\n<a name="#{cur}"></a>#{LeftSq}#{cur}#{RightSq} [#{display_link}](#{link} "#{link}") #{description}\n\n!
+      }.join
     end
-      
-    # wraps things in a div. If no id given, no div.
-    def self.divit( id=nil )
-      id ? "<div markdown='1' id='#{id}'>#{ yield }</div>" : yield
+
+
+    # Wraps things in a div. If no id given, no div.
+    # @param [#to_s] id The ID attribute for the div.
+    def self.divit( id )
+      "<div markdown='1' id='#{id}'>#{ yield }</div>"
     end
 
     # HTML code for [
--- spec/link_reffing_spec.rb
+++ spec/link_reffing_spec.rb
@@ -8,17 +8,41 @@ module MarkdownFilters
   describe LinkReffing do
     context "Given some text" do
       context "With a link to be reffed in it" do
+        shared_examples "outputting links" do
+          it { should_not be_nil }
+          it { should == expected }
+        end
 
-        let(:content) { "The UtterFAIL website[[http://utterfail.info|UtterFAIL!]] is good." }
-        let(:expected) { %Q$The UtterFAIL website[&#8304;](#0 "Jump to reference") is good.\n<div markdown='1' id='reflinks'>\n<a name="0"></a>&#91;0&#93; [http://utterfail.info](http://utterfail.info "http://utterfail.info") UtterFAIL!\n\n</div>$ }
+        let(:content) { "The UtterFAIL website[[http://utterfail.info|UtterFAIL!]] is good. My blog[[http://iainbarnett.me.uk|My blog]] is also good." }
 
-        subject { MarkdownFilters::LinkReffing.run content }
-        it { should_not be_nil }
-        it { should == expected }
+        context "and no options" do
+          subject { MarkdownFilters::LinkReffing.run content }
+          let(:expected) { s = <<HTML
+The UtterFAIL website[&#8304;](#0 "Jump to reference") is good. My blog[&sup1;](#1 "Jump to reference") is also good.
+<div markdown='1' id='reflinks'>
+<a name="1"></a>&#91;1&#93; [http://utterfail.info](http://utterfail.info "http://utterfail.info") UtterFAIL!
+
+
+<a name="2"></a>&#91;2&#93; [http://iainbarnett.me.uk](http://iainbarnett.me.uk "http://iainbarnett.me.uk") My blog
+
+</div>
+HTML
+            s.strip
+          }
+          include_examples "outputting links"
+        end
+        context "and an option not to ref the link" do
+          subject { MarkdownFilters::LinkReffing.run content }
+          pending
+        end
+        context "and an option to output a link 'normally'" do
+          subject { MarkdownFilters::LinkReffing.run content }
+          pending
+        end
       end # context
       
       context "With no link to be reffed in it" do
-        let(:content) { %Q$The[UtterFAIL website](http://utterfail.info/ "UtterFAIL!") is good.$ }
+        let(:content) { %Q$The [UtterFAIL website](http://utterfail.info/ "UtterFAIL!") is good.$ }
         let(:expected) { content }
         subject { MarkdownFilters::LinkReffing.run content }
         it { should_not be_nil }
