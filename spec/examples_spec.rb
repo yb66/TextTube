245d51c7ded89fc39a6406c585124c111198910e
Iain Barnett <iainspeed@gmail.com>
2015-06-14 02:48:59 +0100

    Specs for: options shouldn't be in Base, only subclasses, Base is 'pure'; method options override instance options, which override class options.

--- spec/examples_spec.rb
+++ spec/examples_spec.rb
@@ -5,6 +5,8 @@ require_relative "../lib/texttube/filterable.rb"
 
 module TextTube # for convenience
 
+TimeNow = "2060" # According to Sir Isaac Newton's research of the Bible, the world will end in this year!
+
 describe "Example usage" do
 	before :all do
 		module AFilter
@@ -27,6 +29,18 @@ describe "Example usage" do
 			end
 		end
 
+    module AnotherFilter
+      extend TextTube::Filterable
+
+      filter_with :copyright do |text|
+        text << " ©#{TimeNow}. "
+      end
+
+      filter_with :number do |text,options|
+        text * options[:times].to_i
+      end
+    end
+
 		class NeuS < TextTube::Base
 			register BFil
 			register AFilter
@@ -36,43 +50,120 @@ describe "Example usage" do
 				end
 			end
 		end
+
+    class NewerS < TextTube::Base
+			register BFil
+			register AFilter
+			register do
+				filter_with :dashes do |text|
+					"---#{text}---"
+				end
+			end
+		end
+
+    class FutureString < TextTube::Base
+      register AnotherFilter
+      register do
+        filter_with :read_more do |text,options|
+          text << options[:teaser]
+        end
+      end
+    end
 	end # before
 
 	context "Simple" do
 		let(:n) { NeuS.new "abc" }
+    let(:the_no_options_filtered_result){ "---a b ca b ca b ca b ca b ca b c---" }
 		subject { n }
 		it { should == "abc" }
 
 		context "filtering" do
 			context "With no arguments" do
 				subject { n.filter }
-				it { should == "---a b ca b ca b ca b ca b ca b c---" }
+				it { should == the_no_options_filtered_result }
 			end
 			context "Given a specific filter" do
 				subject { n.filter :spacial }
 				it { should == "a b c" }
+        it { should_not == the_no_options_filtered_result }
 			end
 			context "Given several specific filters" do
 				subject { n.filter :spacial, :dashes, :spacial }
 				it { should == "- - - a   b   c - - -" }
+        it { should_not == the_no_options_filtered_result }
 			end
 		end
 	end
-	context "with global options" do
+	context "with options passed to the instance" do
+	  context "To set the order" do
+      let(:n) { NeuS.new "abc", :order=>[:spacial, :dashes, :spacial] }
+      let(:the_no_options_filtered_result){ "- - - a   b   c - - -" }
+      subject { n }
+      it { should == "abc" }
+      context "filtered" do
+        subject { n.filter }
+        it { should == the_no_options_filtered_result }
+        context "and then given method options" do
+          subject { n.filter :order=>[:triple, :dashes, :spacial] }
+          it { should == "- - - a b c a b c a b c - - -" }
+          it { should_not == the_no_options_filtered_result }
+        end
+      end
+      context "with a different instance" do
+        let(:m) { NeuS.new "def" }
+        subject { m }
+        it { should == "def" }
+        context "filtered" do
+          subject { m.filter }
+          it { should_not == "- - - d   e   f - - -" }
+          it { should_not == the_no_options_filtered_result }
+        end
+        context "and given instance options" do
+          let(:k) { NeuS.new "abc", :order=>[:dashes, :double, :spacial] }
+          subject { k }
+          it { should == "abc" }
+          context "filtered" do
+            subject { k.filter }
+            it { should == "- - - a b c - - - - - - a b c - - -" }
+            it { should_not == the_no_options_filtered_result }
+          
+            context "and then given method options" do
+              subject { k.filter :order=>[:triple, :dashes, :spacial] }
+              it { should == "- - - a b c a b c a b c - - -" }
+              it { should_not == the_no_options_filtered_result }
+            end
+          end
+        end
+      end
+      context "with a different class" do
+        let(:n) { NewerS.new "abc" }
+        subject { n }
+        it { should == "abc" }
+        context "filtered" do
+          subject { n.filter }
+          it { should == "---a b ca b ca b ca b ca b ca b c---" }
+          it { should_not == the_no_options_filtered_result }
+        end        
+      end
+    end
+	end
+	context "with class options" do
 	  context "To set the order" do
 	    before :all do
 	      NeuS.options.merge! :order=>[:spacial, :dashes, :spacial]
 	    end
       let(:n) { NeuS.new "abc" }
+      let(:the_no_options_filtered_result){ "- - - a   b   c - - -" }
       subject { n }
       it { should == "abc" }
       context "filtered" do
         subject { n.filter }
-        it { should == "- - - a   b   c - - -" }
+        it { should == the_no_options_filtered_result }
       end
-      context "and given instance options" do
+      context "and given method options" do
         subject { n.filter :order=>[:dashes, :double, :spacial] }
         it { should == "- - - a b c - - - - - - a b c - - -" }
+        it { should_not == the_no_options_filtered_result }
       end
       context "with a different instance" do
         let(:m) { NeuS.new "def" }
@@ -83,16 +174,31 @@ describe "Example usage" do
           it { should == "- - - d   e   f - - -" }
         end        
       end
-    end
-	end
-	context "with options passed to the instance" do
-	  context "To set the order" do
-      let(:n) { NeuS.new "abc", :order=>[:spacial, :dashes, :spacial] }
-      subject { n }
-      it { should == "abc" }
-      context "filtered" do
-        subject { n.filter }
-        it { should == "- - - a   b   c - - -" }
+      context "with a different class" do
+        let(:n) { NewerS.new "abc" }
+        subject { n }
+        it { should == "abc" }
+        context "filtered" do
+          subject { n.filter }
+          it { should == "---a b ca b ca b ca b ca b ca b c---" }
+          it { should_not == the_no_options_filtered_result }
+        end        
+      end
+      context "and given instance options" do
+        let(:m) { NeuS.new "abc", :order=>[:dashes, :double, :spacial] }
+        subject { m }
+        it { should == "abc" }
+        context "filtered" do
+          subject { m.filter }
+          it { should == "- - - a b c - - - - - - a b c - - -" }
+          it { should_not == the_no_options_filtered_result }
+          
+          context "and then given method options" do
+            subject { m.filter :order=>[:triple, :dashes, :spacial] }
+            it { should == "- - - a b c a b c a b c - - -" }
+            it { should_not == the_no_options_filtered_result }
+          end
+        end
       end
     end
 	end
